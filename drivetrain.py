a71a27faa386d41866035ca97867d912a2240db2
y33t3r3r33 <dumasdd9@gmail.com>
2025-01-08 20:55:00 -0600

    Things happened

--- drivetrain.py
+++ drivetrain.py
@@ -45,3 +45,69 @@ class DriveTrain():
         self.brd = rev.CANSparkMax(4, rev.CANSparkMax.MotorType.kBrushless)
         self.fld = rev.CANSparkMax(6, rev.CANSparkMax.MotorType.kBrushless)
         self.frd = rev.CANSparkMax(5, rev.CANSparkMax.MotorType.kBrushless)
+
+        self.blr.setOpenLoopRampRate(0.2)
+        self.brr.setOpenLoopRampRate(0.2)
+        self.flr.setOpenLoopRampRate(0.2)
+        self.frr.setOpenLoopRampRate(0.2)
+
+        self.bld.setOpenLoopRampRate(0.2)
+        self.brd.setOpenLoopRampRate(0.2)
+        self.fld.setOpenLoopRampRate(0.2)
+        self.frd.setOpenLoopRampRate(0.2)
+
+        self.bldenc = self.bld.getEncoder(rev.SparkRelativeEncoder.Type.kHallSensor, 42)
+        self.brdenc = self.brd.getEncoder(rev.SparkRelativeEncoder.Type.kHallSensor, 42)
+        self.fldenc = self.fld.getEncoder(rev.SparkRelativeEncoder.Type.kHallSensor, 42)
+        self.frdenc = self.frd.getEncoder(rev.SparkRelativeEncoder.Type.kHallSensor, 42)
+
+        self.blenc = ctre.hardware.CANcoder(12)
+        self.brenc = ctre.hardware.CANcoder(11)
+        self.flenc = ctre.hardware.CANcoder(13)
+        self.frenc = ctre.hardware.CANcoder(10)
+
+        self.lcs = ChassisSpeeds(0, 0, 0) #last chassis speed
+
+        rkp = 2
+        rki = 0
+        rkd = 0.1
+        self.blPID = controller.PIDController(rkp, rki, rkd)
+        self.blPID.enableContinuousInput(-0.5, 0.5)
+        self.blPID.setSetpoint(0.0)
+        self.brPID = controller.PIDController(rkp, rki, rkd)
+        self.brPID.enableContinuousInput(-0.5, 0.5)
+        self.brPID.setSetpoint(0.0)
+        self.flPID = controller.PIDController(rkp, rki, rkd)
+        self.flPID.enableContinuousInput(-0.5, 0.5)
+        self.flPID.setSetpoint(0.0)
+        self.frPID = controller.PIDController(rkp, rki, rkd + -0.1)
+        self.frPID.enableContinuousInput(-0.5, 0.5)
+        self.frPID.setSetpoint(0.0)
+
+        self.rrPID = controller.PIDController(0.001, 0, 0)
+
+        dkp = 0.01
+        dki = 0
+        dkd = 0
+
+        self.bldPID = controller.ProfiledPIDContoller(dkp, dki, dkd,
+                                                      wpimath.trajectory.TrapezoidProfile.Contraints(3, 10))
+        self.brdPID = controller.ProfiledPIDController(dkp, dki, dkd,
+                                                       wpimath.trajectory.TrapezoidProfile.Contraints(3, 10))
+        self.fldPID = controller.ProfiledPIDController(dkp, dki, dkd,
+                                                      wpimath.trajectory.TrapezoidProfile.Contraints(3, 10))
+        self.frdPID = controller.ProfiledPIDController(dkp, dki, dkd,
+                                                       wpimath.trajectory.TrapezoidProfile.Contraints(3, 10))
+
+        self.gyro = navx.AHRS.create_i2c(wpilib.I2C.Port.kMXP)
+
+        self.gyro.enableLogging(True)
+
+        bll = Translation2d(-2.08, -2.08)
+        brl = Translation2d(-2.08, 2.08)
+        fll = Translation2d(2.08, 2.75)
+        frl = Translation2d(2.08, -2.75)
+
+        self.kinematics = SwerveDrive4Kinematics(
+            bll, brl, fll, frl
+        )
---
+++ robotcontainer.py
@@ -0,0 +1,5 @@
+from drivetrain import DriveTrain
+
+class RobotContainer:
+    def __init__(self) -> None:
+        self.drivetrain = DriveTrain()
