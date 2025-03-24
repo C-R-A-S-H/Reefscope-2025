097ec9e919309e6596f7c96b3209989c5c66abca
y33t3r3r33 <dumasdd9@gmail.com>
2025-03-23 17:51:11 -0700

    got coral detection made

--- Components/autoaim.py
+++ Components/autoaim.py
@@ -5,11 +5,14 @@ import numpy as np
 global algaeVisible
 global algaeYaw
 
+global coralVisable
+
 class autoaim():
     def __init__(self):
-        self.alcam = PhotonCamera("Logitech,_ink._Webcam_C270")
+        self.alcam = PhotonCamera("alcam")
+        self.corcam = PhotonCamera("corcam")
 
-    def aiming(self): 
+    def aimingal(self): 
         global algaeYaw
         global algaeVisible
         algaeYaw = 0.0
@@ -33,6 +36,31 @@ class autoaim():
                 x, y, w, h = cv2.boundingRect(largest_contour)
                 algaeVisible = True
                 algaeYaw = self.calculateYaw(x, x + w, frame.shape[1])
+
+    def aimingcor(self):
+        global coralVisable
+
+        coralVisable = False
+        results = self.corcam.getAllUnreadResults()
+        if len(results) > 0:
+            result = results[-1]
+            frame = result.getFrame()
+
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            lower_coral = np.array([0,0,200])
+            upper_coral = np.array([180,55,255])
+
+            mask = cv2.inRange(hsv, lower_coral, upper_coral)
+
+            contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if contours:
+                largest_contour = max(contours, key=cv2.contourArea)
+                x, y, w, h = cv2.boundingRect(largest_contour)
+                coralVisable = True
+                coralYaw = self.calculateYaw(x, x + w, frame.shape[1])
+
 
     def calculateYaw(self, startX, endX, frameWidth):
         centerX = (startX + endX) / 2.0
